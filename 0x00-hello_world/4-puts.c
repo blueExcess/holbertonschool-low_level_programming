3319d673ed69286de7cb0cd09f2543b62e509b10
Nate Petersen <nate.petersen@me.com>
2019-02-06 17:40:03 +0000

    finished 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+/* 4. Hello, puts */
+
+int main(void) {
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+/* 5. Hello, printf */
+
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return 0;
+}
