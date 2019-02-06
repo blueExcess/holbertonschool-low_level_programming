3dc724f79ba9ec466ca1814729d44ca00949f6fa
Nate Petersen <nate.petersen@me.com>
2019-02-06 18:51:09 +0000

    fix for 5 and finished 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,17 @@
 /* 6. Size is not grandeur, and territory does not make a nation */
 
-int main (void) {
-	
+/**
+ * main - will return sizeof various data types
+ *
+ * Return: will return 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zd byte(s)\n", sizeof(char));
+	printf("Size of an int: %zd byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zd byte(s)\n", sizeof(float));
+
+	return (0);
+}
