24233baf698ac65971b927e9bbe97ae52ccbc147
Nate Petersen <nate.petersen@me.com>
2019-02-06 21:17:50 +0000

    finished 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/* 9. Print out quote w/o using printf or puts */
+
+#include <stdio.h>
+
+/**
+ * main - will print as error msg
+ *
+ * Return: will give 1
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
