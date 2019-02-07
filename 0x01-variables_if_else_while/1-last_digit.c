62b863cf90223258d16488293c125d05d67c0b3e
Nate Petersen <nate.petersen@me.com>
2019-02-07 18:16:31 +0000

    fix to 1 - forgot \n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,6 +21,6 @@ if (x > 5)
 else if (x == 0)
 	printf("The last digit of %d is %d and is 0\n", n, x);
 else if (x < 6 && x != 0)
-	printf("The last digit of %d is %d and is less than 6 and not 0", n, x);
+	printf("The last digit of %d is %d and is less than 6 and not 0\n", n, x);
 return (0);
 }
