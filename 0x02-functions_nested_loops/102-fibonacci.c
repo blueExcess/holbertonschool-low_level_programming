0e5a49304ef4e184a4a61d72372fc70c5df1b00e
Nate Petersen <nate.petersen@me.com>
2019-02-13 04:48:50 +0000

    changed starting point of 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,7 +17,7 @@ int main(void)
 
 	for (i = 2; i < 51; i++)
 		fib[i] = fib[i - 2] + fib[i - 1];
-	for (i = 1; i < 51; i++)
+	for (i = 2; i < 51; i++)
 	{
 		if (i == 50)
 			printf("%lu\n", fib[i]);
