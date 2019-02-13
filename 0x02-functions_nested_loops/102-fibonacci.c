87fbb9c077f5ab7f9d3728911e2a2b6a3220c922
Nate Petersen <nate.petersen@me.com>
2019-02-13 04:35:17 +0000

    fix to 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,8 +17,8 @@ int main(void)
 
 	for (i = 2; i < 51; i++)
 		fib[i] = fib[i - 2] + fib[i - 1];
-	for (i = 0; i < 51; i++)
-		printf("%lu\n", fib[i]);
+	for (i = 2; i < 51; i++)
+		printf("%lu, ", fib[i]);
 	printf("\n");
 	return (0);
 }
