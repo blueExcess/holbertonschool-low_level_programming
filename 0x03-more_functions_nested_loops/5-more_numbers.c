a9ca55ae30dbd04e0e9676c12b8f878c259a8445
Nate Petersen <nate.petersen@me.com>
2019-02-15 21:19:16 +0000

    Fix betty on 5

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for(y = 0; y < 15; y++)
+		for (y = 0; y < 15; y++)
 		{
-			if(y > 9)
+			if (y > 9)
 				_putchar('0' + (y / 10));
 			_putchar('0' + (y % 10));
 		}
