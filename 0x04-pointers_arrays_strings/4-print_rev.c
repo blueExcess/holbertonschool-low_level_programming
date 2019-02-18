8da8f4ee6056d5f5f4fd9f62b12e98f3575cdb9f
Nate Petersen <nate.petersen@me.com>
2019-02-18 21:36:38 +0000

    completed 3 and 4

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+/* 4. Print string in reverse */
+
+#include "holberton.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string to reverse
+ *
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int length = 0, i;
+
+	while (*s)
+	{
+		s++;
+		length++;
+	}
+
+	s--;
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar(10);
+}
