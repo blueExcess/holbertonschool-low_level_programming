8ffb818260e52d14bb0a54d1cb494ea07396e4ac
Nate Petersen <nate.petersen@me.com>
2019-02-19 19:20:51 +0000

    changed if/else of 8 to correctly handle 0

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -16,12 +16,15 @@ void print_array(int *a, int n)
 
 	if (n == 0)
 		printf("\n");
-
-	for (i = 0; i < n; i++)
+	else
 	{
-		if (i < n - 1)
-			printf("%d, ", a[i]);
-		else
-			printf("%d\n", a[i]);
+		for (i = 0; i < n; i++)
+		{
+			if (i < n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d", a[i]);
+		}
+		printf("\n");
 	}
 }
