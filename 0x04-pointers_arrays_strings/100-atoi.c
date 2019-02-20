1f6282ca243edf8498c48a1186957a90cd05ad20
Nate Petersen <nate.petersen@me.com>
2019-02-20 04:29:39 +0000

    draft of 10

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+/* 10 (advanced) - mimic function of atoi() */
+
+#include "holberton.h"
+
+/**
+ * _atoi - convert string to numbers
+ * @s: string to convert
+ *
+ * Return: int from string
+ */
+int _atoi(char *s)
+{
+	int x;
+	char arr[];
+
+	while (*s)
+	{
+		x++;
+		if (*s >= 48 && *s <= '9')
+			conv[i] = *s;
+	}
+}
+
+/* need to check if each value = ascii number, if yes */
+/* then need to enter into second array. From there */
+/* convert via lookup table or switch statement. */
