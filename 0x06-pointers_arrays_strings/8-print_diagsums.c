9c4fceb07fb1e5339125da81cc049692a44877e4
Nate Petersen <nate.petersen@me.com>
2019-02-26 00:36:56 +0000

    completed 7 (8-)

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+/* 7. print the sums of two diagonals */
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sum and print diagonals
+ * @a: matrix to sum
+ * @size: size of matrix (squares)
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned long int sum1 = 0, sum2 = 0;
+	int x;
+
+	for (x = 0; x < size * size; x += (size + 1))
+		sum1 += a[x];
+	for (x = size - 1; x < (size * size) - (size - 1); x += (size - 1))
+		sum2 += a[x];
+	printf("%lu, %lu\n", sum1, sum2);
+}
