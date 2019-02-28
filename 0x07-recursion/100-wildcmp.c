4064c60415205cac8d45ba7f1892b14c9e0a3014
Nate Petersen <nate.petersen@me.com>
2019-02-28 04:34:21 +0000

    first draft of 100- (#8, advanced)

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,34 @@
+/* 9 (advanced) check if two strings */
+#include "holberton.h"
+
+/**
+ * compare - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 1 for same, 0 for not
+ */
+int compare(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+		return (1); /* should be 0 */
+	else if (*s1)
+		return (compare(++s1, ++s2));
+	return (1);
+}
+
+
+/**
+ * wildcmp - match two strings and pretend to do a wildcard
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: int
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (compare(s1, s2));
+	return (1);
+
+}
