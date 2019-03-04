16086a7ae2fcaaf508ea26e6a0c27db778509529
Nate Petersen <nate.petersen@me.com>
2019-03-04 14:23:57 +0000

    completed 100-wildcmp

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -27,8 +27,15 @@ int compare(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
+	if (*s1 == 0)
+	{
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+	return (*s2 == 0);
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	if (*s1 == *s2)
-		return (compare(s1, s2));
-	return (1);
-
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
