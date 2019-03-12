a4aaca55e6535f759d59b8b1b94cf1dc2df3f61e
Nate Petersen <nate.petersen@me.com>
2019-03-12 20:34:36 +0000

    added free for cpN and cpO - fixed indirect mem loss

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -20,7 +20,7 @@ char *countAndPoint(char *s)
 	{
 		x++;
 	}
-	p = malloc(sizeof(*p) * x + 1);
+	p = malloc(sizeof(*p) * (x + 1));
 	if (p == NULL)
 		return (NULL);
 	while (s[z])
@@ -45,31 +45,35 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cpN, *cpO;
 	dog_t *ret;
 
-	ret = malloc(sizeof(ret));
+	ret = malloc(sizeof(*ret));
 	if (ret == NULL)
 		return (NULL);
-	ret->name = NULL;
+	/* ret->name = NULL; */
 	if (name)
 	{
 		cpN = countAndPoint(name);
 		if (cpN == NULL)
 		{
+			free(cpN);
 			free(ret);
 			return (NULL);
 		}
 		ret->name = cpN;
+		free(cpN);
 	}
-	ret->owner = NULL;
+	/* ret->owner = NULL; */
 	if (owner)
 	{
 		cpO = countAndPoint(owner);
 		if (cpO == NULL)
 		{
+			free(cpO);
 			free(ret->name);
 			free(ret);
 			return (NULL);
 		}
 		ret->owner = cpO;
+		free(cpO);
 	}
 	if (age > 0.0)
 		ret->age = age;
