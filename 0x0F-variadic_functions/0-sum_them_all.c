be80aedaf5e8aa1cf38a14d20a94e170d1e7dfa7
Nate Petersen <nate.petersen@me.com>
2019-03-15 22:31:16 +0000

    added null check to 0

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,9 @@ int sum_them_all(const unsigned int n, ...)
 	int i, sum;
 	va_list valist;
 
+	if (n == 0)
+		return (0);
+
 	va_start(valist, n);
 	for (i = 0; i < num; i++)
 		sum += va_arg(valist, int);
