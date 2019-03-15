3418e36a59b1fb2cc27d6788afccfeb4bef9555e
Nate Petersen <nate.petersen@me.com>
2019-03-15 22:25:53 +0000

    adding file 0x0F and testing 0

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+/* 0. return the sum of all parameters */
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all parameters
+ * @n: number of args
+ *
+ * Return: sum of thingies
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum;
+	va_list valist;
+
+	va_start(valist, n);
+	for (i = 0; i < num; i++)
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x0F-variadic_functions/README.md
@@ -0,0 +1,3 @@
+#0x0F Variadic Functions
+
+Hooray! Go Batman GO!
