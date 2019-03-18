04470e71e96c80c63305fad5bf4f5ed8cc36b766
Nate Petersen <nate.petersen@me.com>
2019-03-18 03:29:57 +0000

    added comments to 100

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,18 @@
+;; 4. (advanced - 100) Print to terminal using assembly like a boss
+;; to compile:
+;; nasm -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o -o hello
+
 	global main
 
 	section .text
 main:
-	mov	rax, 1
-	mov	rdi, 1
-	mov	rsi, message
-	mov	rdx, 17
+	mov	rax, 1		;R0
+	mov	rdi, 1		;R7
+	mov	rsi, message	;R6
+	mov	rdx, 17		;R2
 	syscall
-	mov	rax, 60
-	xor	rdi, rdi
+	mov	rax, 60		;R0
+	xor	rdi, rdi	;R7
 	syscall
 
 	section .data
