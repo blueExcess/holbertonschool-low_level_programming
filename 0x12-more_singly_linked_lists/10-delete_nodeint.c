9932b5b57b60bdaa7e995f6867b8da1d648d76cf
Nate Petersen <nate.petersen@me.com>
2019-03-27 04:04:31 +0000

    changed if statement for index == 0

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	tgt = *head;
 	if (index > 0)
 	{
-		for (index; index && ind->next; index++)
+		for (index--; index && ind->next; index++)
 		{
 			tgt = tgt->next;
 			if (index == 1)
@@ -35,6 +35,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
+		*head = tgt->next;
+		free(tgt);
 		return (1);
 	}
 	return (-1);
