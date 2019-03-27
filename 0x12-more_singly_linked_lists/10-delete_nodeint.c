d826285d0e0e8718fd51eafe33fdee6ce1c52dc9
Nate Petersen <nate.petersen@me.com>
2019-03-27 04:01:21 +0000

    draft of 10:

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,3 +10,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *ind, *tgt;
+
+	if (head == NULL)
+		return (-1);
+	if (*head == NULL)
+		return (-1);
+	ind = *head;
+	tgt = *head;
+	if (index > 0)
+	{
+		for (index; index && ind->next; index++)
+		{
+			tgt = tgt->next;
+			if (index == 1)
+				continue;
+			ind = tgt->next;
+		}
+		if (index > 0)
+			return (-1);
+		ind->next = tgt->next;
+		free(tgt);
+		return (1);
+	}
+	else
+	{
+		return (1);
+	}
+	return (-1);
+}
