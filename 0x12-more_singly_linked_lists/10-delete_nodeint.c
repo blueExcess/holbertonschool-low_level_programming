e5e0f196a71977549c2934867e21afa3a7fe59da
Nate Petersen <nate.petersen@me.com>
2019-03-27 04:08:45 +0000

    change to ind in for loop

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			tgt = tgt->next;
 			if (index == 1)
 				continue;
-			ind = tgt->next;
+			ind = ind->next;
 		}
 		if (index > 0)
 			return (-1);
