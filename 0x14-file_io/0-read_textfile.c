bf0234cf23d5f6b1e28dd18cf127c06323cf078d
Nate Petersen <nate.petersen@me.com>
2019-04-03 04:05:42 +0000

    draft of 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -11,20 +11,32 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd = 0;
-	ssize_t ret = 0, bytesRead, bytesReadTotal = 0;
+	ssize_t ret = 0, bytesRead, bytesWritten;
 	char buffer[BUFFSIZE];
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
 
-	fd = open(*filename, O_RDONLY);
-
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
 	do {
-		bytesRead = read(fd, buffer, letters);
-		bytesReadTotal += bytesRead;
-		ret = write_all(fd, buffer, BUFFSIZE);
-
+		bytesRead = read(fd, buffer, BUFFSIZE);
+		if (bytesRead < 1)
+			break;
+		/* don't understand why bR is changing at this line - ask Sam */
+		bytesRead = ret + bytesRead < letters ? (size_t)bytesRead : letters - ret;
+		bytesWritten = write_all(STDOUT_FILENO, buffer, bytesRead);
+		if (bytesWritten < 0)
+			break;
+		ret += bytesWritten;
+	} while (ret < letters && bytesWritten == bytesRead);
+	if (bytesRead < 0 || bytesWritten < 0)
+		return (0);
+	close (fd);
+	return (ret);
 }
+
 
 
 
@@ -38,14 +50,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
  */
 ssize_t write_all(int file, char *buffer, size_t count)
 {
-	size_t write_last, write_total = 0;
+	ssize_t write_last, write_total = 0;
 
 	if (buffer == NULL || file < 0 || count < 1)
 		return (-1);
 	do {
 		write_last = write(file, buffer + write_total, count);
 		write_total += write_last;
-	} while (write_total != count && write_last > 0);
+	} while (write_total < count && write_last > 0);
 	if (write_last < 0)
 		return (-1);
 	return (write_total);
