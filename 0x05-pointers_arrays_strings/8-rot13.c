c0d7e21f16ffb6e1aa500e9cd60618eae301435c
Nate Petersen <nate.petersen@me.com>
2019-04-04 20:58:29 +0000

    completed 10 and fixed bugs on 8

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,30 +1,19 @@
-/* 8. function to encode string using rot13 */
-#include "holberton.h"
+/* 8. create function to change string to rot13 */
 
 /**
  * rot13 - encode string using rot13
  * @str: string to encode
  *
- * Return: encoded string
+ * Return: pointer to encoded string
  */
 char *rot13(char *str)
 {
-	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot[] =	 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char *s = str, *let = letters, *r = rot;
-	int position = 0;
+	char *s = str;
+	char const *rot =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM123456nopqrstuvwxyzabcdefghijklm";
 
-	while (*s)
-	{
-		for (position = 0; *let; let++, r++, position++)
-		{
-			if (*s == *let)
-				*s = *r;
-				break;
-		}
-		let = letters;
-		r = rot;
-		s++;
-	}
+	for (; *s; s++)
+		if ((*s >= 'A' && *s <= 'Z') || (*s >= 'a' && *s <= 'z'))
+			*s = rot[*s - 'A'];
 	return (str);
 }
