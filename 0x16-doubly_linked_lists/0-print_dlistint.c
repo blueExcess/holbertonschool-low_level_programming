c05f43123b7f8f4c4d42a5fadf890c064ff1c640
Nate Petersen <nate.petersen@me.com>
2019-05-03 20:28:54 +0000

    upload template for tasks and completed 1

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+/* 0. function to print value of all nodes of dll */
+#include "lists.h"
+
+/**
+ * print_dlistint - print value of all nodes
+ * @h: pointer to start of list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (1);
+
+	for (; h; h = h->next, i++)
+		printf("%d\n", h->n);
+
+	return (i);
+}
