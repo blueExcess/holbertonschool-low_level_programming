35aaf9786303c1ffbbff912f27cb48d9d6001aaa
Nate Petersen <nate.petersen@me.com>
2019-05-04 03:39:14 +0000

    complete 6

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,20 @@
+/* 6. func to return sum of all nodes in DLL */
 #include "lists.h"
 
 /**
- * sum_dlistint -
+ * sum_dlistint - return sum of all nodes
+ * @head: start of DLL
  *
- * Return: 
+ * Return: 0 if empty, otherwise sum
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+	dlistint_t *seek = head;
 
+	if (!head)
+		return (sum);
+	for (; seek; seek = seek->next)
+		sum += seek->n;
+	return (sum);
 }
