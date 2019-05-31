de56218c3294357e126c3001f80da0acb20165c9
Nate Petersen <nate.petersen@me.com>
2019-05-31 21:02:42 +0000

    fix betty issues

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -11,7 +11,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int ind = 0;
-	ind = hash_djb2(key) % size;
 
+	ind = hash_djb2(key) % size;
 	return (ind);
 }
