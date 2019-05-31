65ccfb9a58a78c01df597d096440d8cf7415a68c
Nate Petersen <nate.petersen@me.com>
2019-05-31 19:54:08 +0000

    setup

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -
+ *
+ * Return: pointer to the hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_s *table;
+
+	table = calloc(
+}
