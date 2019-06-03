93f5d95431a27130fe5680177313e56aa2d1eb22
Nate Petersen <nate.petersen@me.com>
2019-06-03 20:46:31 +0000

    draft of 6

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -9,5 +9,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	
+	hash_node_t *s, *t, *n;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+	s = ht->array[0];
+	for (; i < ht->size; i++, s = ht->array[i])
+		if (s)
+		{
+			t = s;
+			while (t)
+			{
+				n = t->next;
+				free(t->key);
+				free(t->value);
+				free(t);
+				t = n;
+			}
+		}
+	free(ht->array);
+	free(ht);
 }
