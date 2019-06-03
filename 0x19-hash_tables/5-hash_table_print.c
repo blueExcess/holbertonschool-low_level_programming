ef2a595807da9370c6658dc239d4a200f1e5d35d
Nate Petersen <nate.petersen@me.com>
2019-06-03 19:29:59 +0000

    complete 5

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,11 +1,33 @@
+/* 5. function to print hash map. */
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - print out entire hash map.
+ * @ht: hash table
  *
- * Return: 
+ * Return: print out list - if ht == null, print nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	int i = 0, j = 0;
+	hash_node_t *s;
+
+	if (ht == NULL)
+		return;
+	for (j = ht->size - 1; ht->array[j] == NULL; j--)
+		;
+	s = ht->array[0];
+	putchar('{');
+	for (; i <= j; i++, s = ht->array[i])
+	{
+		for (; s; s = s->next)
+		{
+			if (s && i != j)
+				printf("'%s': '%s, '", s->key, s->value);
+			else if (s && i == j)
+				printf("'%s': '%s'", s->key, s->value);
+		}
+	}
+	puts("}");
 
 }
