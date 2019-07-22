96953215d4258477834c76a33b18b0d62dd9eae5
Nate Petersen <nate.petersen@me.com>
2019-07-22 18:52:09 +0000

    complete task 2 - add/replace node on right

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,11 +1,38 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -
+ * binary_tree_insert_right - insert node as right child of parent.
+ * @parent: parent node.
+ * @value: value of new node.
  *
- * Return: 
+ * Return: newly created node, NULL on failure.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new, *old;
+
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->right = NULL;
+
+	if (parent->right != NULL)
+	{
+		old = parent->right;
+		new->right = old;
+		old->parent = new;
+	}
+
+	parent->right = new;
+
+	return (new);
 
 }
