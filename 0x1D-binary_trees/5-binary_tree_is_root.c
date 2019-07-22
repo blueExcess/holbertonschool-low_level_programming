c80082cda9b03e79a0fce8f0ef15cc6fee77702c
Nate Petersen <nate.petersen@me.com>
2019-07-22 19:51:10 +0000

    task 5 - check if node is root

--- 0x1D-binary_trees/5-binary_tree_is_root.c
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root -
+ * binary_tree_is_root - check if node is root node.
+ * @node: node to check.
  *
- * Return: 
+ * Return: 1 if root, 0 otherwise.
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-
+	if (node->parent == NULL)
+		return (1);
+	return (0);
 }
