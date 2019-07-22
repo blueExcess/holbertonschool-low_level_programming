b95df98f977cc110c5ed16c3bb43b91251be8301
Nate Petersen <nate.petersen@me.com>
2019-07-22 18:47:24 +0000

    add header (completed) and complete tasks 0 and 1

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,11 +1,37 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -
+ * binary_tree_insert_left - insert node as left child of parent.
+ * @parent: parent node.
+ * @value: value to give new node.
  *
- * Return: 
+ * Return: newly created node, NULL on failure.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new, *old;
 
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		old = parent->left;
+		new->left = old;
+		old->parent = new;
+	}
+
+	parent->left = new;
+
+	return (new);
 }
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -4,6 +4,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/* STRUCTS */
+
+/* basic binary tree */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* binary search tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max binary heap */
+typedef struct binary_tree_s heap_t;
+
+/* PROTOTYPES */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -43,5 +75,7 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
