cbbbcdd6970af4be0d716b719b4f50e209097538
Nate Petersen <nate.petersen@me.com>
2019-07-23 20:21:58 +0000

    complete task 13 - count number of non-leaf nodes

--- 0x1D-binary_trees/13-binary_tree_nodes.c
+++ 0x1D-binary_trees/13-binary_tree_nodes.c
@@ -1,11 +1,24 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes -
+ * binary_tree_nodes - count nodes with at least one child.
+ * @tree: root node.
  *
- * Return: 
+ * Return: number of non-leaves, 0 on failure
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t count = 0;
 
+
+	if (tree == NULL)
+		return (0);
+
+	count = binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right);
+
+	if (tree->left != NULL || tree->right != NULL)
+		count++;
+
+	return (count);
 }
