150ab38b08b009427c73e31be213d3ffcfc8f0e9
Nate Petersen <nate.petersen@me.com>
2019-07-23 17:19:58 +0000

    complete task 10 - measure depth of given node

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth -
+ * binary_tree_depth - find depth of given node.
+ * @tree: node to measure.
  *
- * Return: 
+ * Return: depth of node, 0 on failure.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	int count = 0;
 
+
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	count = binary_tree_depth(tree->parent);
+	return (count + 1);
 }
