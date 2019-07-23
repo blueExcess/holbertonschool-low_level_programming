d8d856ef1185d710f55da8130761ac3c9fb89da9
Nate Petersen <nate.petersen@me.com>
2019-07-23 19:48:29 +0000

    complete task 12 - count number of leaves

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves -
+ * binary_tree_leaves - count number of leaves on BT.
+ * @tree: tree to measure.
  *
- * Return: 
+ * Return: number of leaves, 0 on failure.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 }
