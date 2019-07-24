3b9b76d99e7bdc3c67e07c13adefc66484e17b87
Nate Petersen <nate.petersen@me.com>
2019-07-24 19:25:49 +0000

    complete task 15 - check if tree is full

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -10,5 +10,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	
+	if (tree->right == NULL && tree->left != NULL)
+		return (0);
+	if (tree->right != NULL && tree->left == NULL)
+		return (0);
+
+	if (tree->left != NULL &&
+	    binary_tree_is_full(tree->left) == 0)
+		return (0);
+	if (tree->right != NULL &&
+	    binary_tree_is_full(tree->right) == 0)
+		return (0);
+	return (1);
 }
