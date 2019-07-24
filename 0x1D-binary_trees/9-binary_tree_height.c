3a8a51769f3c85f4eec9db8fc598ae5dcfad565f
Nate Petersen <nate.petersen@me.com>
2019-07-24 18:02:42 +0000

    flip return statements on 9

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -21,7 +21,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right = binary_tree_height(tree->right);
 
 	if (left > right)
-		return (right + 1);
-	else
 		return (left + 1);
+	else
+		return (right + 1);
 }
