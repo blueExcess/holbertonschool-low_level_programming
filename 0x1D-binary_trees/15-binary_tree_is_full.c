fddda58c864651cb42c183cf205a8b7b63167dfa
Nate Petersen <nate.petersen@me.com>
2019-07-24 18:13:57 +0000

    change tree->right to tree->left

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full -
+ * binary_tree_is_full - check if BT is full (all nodes 0 or 2 children).
+ * @tree: root node.
  *
- * Return: 
+ * Return: 1 for true, 0 for false.
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	
 }
--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new->n = value;
-	new->right = NULL;
+	new->left = NULL;
 	new->right = NULL;
 	new->parent = parent;
 
