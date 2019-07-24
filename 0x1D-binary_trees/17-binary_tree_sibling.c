705ba02dc2fb11508aaa1821f3d6c85157239027
Nate Petersen <nate.petersen@me.com>
2019-07-24 18:28:03 +0000

    complete task 17

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -1,11 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling -
+ * binary_tree_sibling - find sibling of given node.
+ * @node: given node.
  *
- * Return: 
+ * Return: pointer to sibling node.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->left)
+	{
+		if (node->parent->right != NULL)
+			return (node->parent->right);
+		else
+			return (NULL);
+	}
+	else
+	{
+		if (node->parent->left != NULL)
+			return (node->parent->left);
+		else
+			return (NULL);
+	}
 }
