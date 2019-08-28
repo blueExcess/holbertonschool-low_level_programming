5836fbda2e60ad6e6ea55cc8f28c12ad825d171b
Nate Petersen <nate.petersen@me.com>
2019-08-28 18:57:13 +0000

    setup and draft of task 0 - linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+/* 0. Linear search - search an array using linear search algorithm */
+
+#include "search_algos.h"
+
+/**
+ * linear_search - search using linear algorithm
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: -1 if not available or null
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	for (; (size_t)i <= size - 1; i++)
+	{
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
